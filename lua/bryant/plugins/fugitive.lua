4799c5a683d50bc4177d5e71e2832a151d0e5e93
Bryant Paz <pazbryant7@outlook.com>
2025-07-11 15:05:46 -0600

    feat(plugins): harpoon and notify plugin added

--- lua/bryant/plugins/fugitive.lua
+++ lua/bryant/plugins/fugitive.lua
@@ -6,7 +6,11 @@ return {
 			'<leader>gd',
 			function()
 				vim.cmd([[Gvdiffsplit!]])
-				vim.notify('Resolving git conflicts', vim.log.levels.INFO)
+				vim.notify(
+					'Resolving git conflicts',
+					vim.log.levels.INFO,
+					{ title = 'Fugitive' }
+				)
 			end,
 			desc = 'Fugitive Diff Split',
 		},
@@ -14,7 +18,7 @@ return {
 			'<leader>go',
 			function()
 				vim.cmd([[diffget //2]])
-				vim.notify('Git ours selected', vim.log.levels.INFO)
+				vim.notify('Git ours selected', vim.log.levels.INFO, { title = "Fugitive" })
 			end,
 			desc = 'Fugitive Ours Changes',
 		},
@@ -22,7 +26,7 @@ return {
 			'<leader>gt',
 			function()
 				vim.cmd([[diffget //3]])
-				vim.notify('Git theirs selected', vim.log.levels.INFO)
+				vim.notify('Git theirs selected', vim.log.levels.INFO, { title = "Fugitive" })
 			end,
 			desc = 'Fugitive Theirs Changes',
 		},
