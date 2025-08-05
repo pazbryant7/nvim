a4f4f045efad030d89012ba0fa4ba88fc680d676
Bryant Paz <pazbryant7@outlook.com>
2025-08-04 19:54:35 -0600

    feat(matchup): update default configuration

--- lua/bryant/plugins/treesitter.lua
+++ lua/bryant/plugins/treesitter.lua
@@ -5,9 +5,6 @@ return {
 	build = ':TSUpdate',
 	config = function()
 		require('nvim-treesitter.configs').setup({
-			matchup = {
-				enable = true,
-			},
 			ensure_installed = {
 				'vimdoc',
 				'javascript',
