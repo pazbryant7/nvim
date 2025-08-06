7c62271ab2fca3e35ee6ded34d8ba30a9ae701bd
Bryant Paz <pazbryant7@outlook.com>
2025-08-05 18:59:06 -0600

    feat(zenmode): update defaults

--- lua/bryant/plugins/zen-mode.lua
+++ lua/bryant/plugins/zen-mode.lua
@@ -5,12 +5,6 @@ return {
 			backdrop = 100, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
 			width = 80, -- width of the Zen window
 		},
-		plugins = {
-			options = {
-				enabled = false,
-			},
-			tmux = { enabled = false }, -- disables the tmux statusline
-		},
 	},
 	keys = {
 		{
