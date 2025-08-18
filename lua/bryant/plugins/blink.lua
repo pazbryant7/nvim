96cd77dd9876b9e17e46e4c89e57603a677d345f
Bryant Paz <pazbryant7@outlook.com>
2025-08-17 18:21:26 -0600

    feat(dial,blink): improve default configurations.

--- lua/bryant/plugins/blink.lua
+++ lua/bryant/plugins/blink.lua
@@ -8,7 +8,7 @@ return {
 			'L3MON4D3/LuaSnip',
 			version = 'v2.*',
 			config = function()
-				require('luasnip.loaders.from_lua').lazy_load({ paths = { './snippets' } })
+				require('luasnip.loaders.from_lua').lazy_load({ paths = { vim.fn.stdpath('config') .. '/snippets' } })
 			end,
 		},
 	},
--- lua/bryant/plugins/dial.lua
+++ lua/bryant/plugins/dial.lua
@@ -12,16 +12,82 @@ return {
 				augend.constant.new({ elements = { 'let', 'const' } }),
 			},
 		})
+
+		-- Helper function to move cursor to start of word after dial operation
+		local function dial_with_cursor_fix(operation, mode)
+			return function()
+				local cursor_pos = vim.api.nvim_win_get_cursor(0)
+				require('dial.map').manipulate(operation, mode)
+
+				-- For normal mode operations, move cursor to start of the changed word
+				if mode == 'normal' or mode == 'gnormal' then
+					-- Move to start of current word
+					vim.cmd('normal! b')
+				end
+			end
+		end
+
+		-- Set up the wrapper functions
+		_G.dial_increment_normal = dial_with_cursor_fix('increment', 'normal')
+		_G.dial_decrement_normal = dial_with_cursor_fix('decrement', 'normal')
+		_G.dial_increment_gnormal = dial_with_cursor_fix('increment', 'gnormal')
+		_G.dial_decrement_gnormal = dial_with_cursor_fix('decrement', 'gnormal')
 	end,
 	keys = {
-    -- stylua: ignore start
-		{ '<C-a>', function() require('dial.map').manipulate('increment', 'normal') end, },
-		{ '<C-x>', function() require('dial.map').manipulate('decrement', 'normal') end, },
-		{ 'g<C-a>', function() require('dial.map').manipulate('increment', 'gnormal') end, },
-		{ 'g<C-x>', function() require('dial.map').manipulate('decrement', 'gnormal') end, },
-    { '<C-a>', mode = "v", function() require('dial.map').manipulate('increment', 'visual') end, },
-		{ '<C-x>', mode = "v", function() require('dial.map').manipulate('decrement', 'visual') end, },
-		{ 'g<C-a>', mode = "v", function() require('dial.map').manipulate('increment', 'gvisual') end, },
-		{ 'g<C-x>', mode = "v", function() require('dial.map').manipulate('decrement', 'gvisual') end, },
+		-- Normal mode with cursor positioning fix
+		{
+			'<C-a>',
+			function()
+				_G.dial_increment_normal()
+			end,
+		},
+		{
+			'<C-x>',
+			function()
+				_G.dial_decrement_normal()
+			end,
+		},
+		{
+			'g<C-a>',
+			function()
+				_G.dial_increment_gnormal()
+			end,
+		},
+		{
+			'g<C-x>',
+			function()
+				_G.dial_decrement_gnormal()
+			end,
+		},
+
+		-- Visual mode (original behavior - cursor positioning less relevant in visual mode)
+		{
+			'<C-a>',
+			mode = 'v',
+			function()
+				require('dial.map').manipulate('increment', 'visual')
+			end,
+		},
+		{
+			'<C-x>',
+			mode = 'v',
+			function()
+				require('dial.map').manipulate('decrement', 'visual')
+			end,
+		},
+		{
+			'g<C-a>',
+			mode = 'v',
+			function()
+				require('dial.map').manipulate('increment', 'gvisual')
+			end,
+		},
+		{
+			'g<C-x>',
+			mode = 'v',
+			function()
+				require('dial.map').manipulate('decrement', 'gvisual')
+			end,
+		},
 	},
 }
