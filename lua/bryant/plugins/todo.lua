8f660695beaa9dda54266e2280f85e4b1ff52474
Bryant Paz <pazbryant7@outlook.com>
2025-08-19 22:31:02 -0600

    feat(todocomments): add new binds

--- lua/bryant/plugins/todo.lua
+++ lua/bryant/plugins/todo.lua
@@ -3,4 +3,20 @@ return {
 	event = 'UIEnter',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	opts = {},
+	keys = {
+		{
+			']t',
+			function()
+				require('todo-comments').jump_next()
+			end,
+			desc = 'Next todo comment',
+		},
+		{
+			'[t',
+			function()
+				require('todo-comments').jump_prev()
+			end,
+			desc = 'Previous todo comment',
+		},
+	},
 }
