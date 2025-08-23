39930deb714538fa058c94d181c7b34996462f4b
Bryant Paz <pazbryant7@outlook.com>
2025-08-22 18:46:40 -0600

    feat(autopairs): use instead of mini.pairs

--- lua/bryant/plugins/lsp/init.lua
+++ lua/bryant/plugins/lsp/init.lua
@@ -98,7 +98,6 @@ return {
 		dependencies = {
 			'nvim-neotest/nvim-nio',
 			'nvim-lua/plenary.nvim',
-			'antoinemadec/FixCursorHold.nvim',
 			'nvim-treesitter/nvim-treesitter',
 
 			-- Language adapters
