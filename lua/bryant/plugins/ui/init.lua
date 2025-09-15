83573d3301f042149573fb588b5e02b5ca4cde6a
Bryant Paz <pazbryant7@outlook.com>
2025-09-14 18:53:05 -0600

    feat(ui): updatintg colorschemes

--- lua/bryant/plugins/ui/init.lua
+++ lua/bryant/plugins/ui/init.lua
@@ -1,8 +1,8 @@
 return {
 	{
-		'catppuccin/nvim',
 		lazy = false,
 		priority = 1000,
+		'catppuccin/nvim',
 		name = 'catppuccin',
 		opts = {
 			flavour = 'mocha', -- latte, frappe, macchiato, mocha
@@ -34,6 +34,31 @@ return {
 			vim.cmd('colorscheme catppuccin-mocha')
 		end,
 	},
+
+	{
+		'Tsuzat/NeoSolarized.nvim',
+		opts = {
+			style = 'dark', -- "dark" or "light"
+			transparent = false, -- true/false; Enable this to disable setting the background color
+			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+			enable_italics = false, -- Italics for different hightlight groups (eg. Statement, Condition, Comment, Include, etc.)
+			styles = {
+				-- Style to be applied to different syntax groups
+				comments = { italic = false },
+				keywords = { italic = false },
+				functions = { bold = false },
+				variables = {},
+				string = { italic = false },
+				underline = true, -- true/false; for global underline
+				undercurl = true, -- true/false; for global undercurl
+			},
+		},
+		config = function(_, opts)
+			require('NeoSolarized').setup(opts)
+			vim.cmd([[ colorscheme NeoSolarized ]])
+		end,
+	},
+
 	{
 		'NvChad/nvim-colorizer.lua',
 		enabled = false,
