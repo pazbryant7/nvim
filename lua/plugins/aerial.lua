4ca2345fc904c24afef5ec59542387c975eb6ce9
Bryant Paz <pazbryant7@outlook.com>
2025-09-25 18:27:29 -0600

    feat(no-neck-pain,aerial): integration between plugins

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -5,19 +5,27 @@ return {
 		'nvim-treesitter/nvim-treesitter',
 	},
 	opts = {
-
+		close_on_select = false,
 		layout = {
-			-- These control the width of the aerial window.
-			-- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-			-- min_width and max_width can be a list of mixed types.
-			-- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
-			max_width = { 60, 0.4 },
+			max_width = { 60, 0.6 },
 			width = nil,
-			min_width = 25,
-			default_direction = 'prefer_right',
+			min_width = 10,
+			default_direction = 'float',
 		},
-
-		autojump = false,
+		close_automatic_events = {
+			'unfocus',
+			'switch_buffer',
+		},
+		autojump = true,
+		show_guides = true,
+		nav = {
+			preview = true,
+			autojump = true,
+		},
+		float = {
+			relative = 'cursor',
+			min_height = { 8, 0.1 },
+		},
 	},
 	keys = {
 		{ '<m-j>', '<cmd>AerialPrev<cr>', desc = 'Aerial Prev Symbol' },
