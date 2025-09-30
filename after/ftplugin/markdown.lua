b143c768d23518c88303fccadae5b22a21093e1b
Bryant Paz <pazbryant7@outlook.com>
2025-09-30 12:01:21 -0600

    feat(obsidian): romove spaces in folder names

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,17 +1,9 @@
-local v = vim
 local o = vim.opt_local
-
-local map = v.keymap.set
 
 if vim.bo.ft ~= 'markdown' then
 	return
 end
 
-local bufnr = vim.api.nvim_get_current_buf()
-
 o.wrap = true
 o.spell = true
 o.textwidth = 80
-
-map('n', '<c-t>', '<cmd>Obsidian search<cr>', { desc = 'Obsidian search', buffer = bufnr, remap = false })
-map('n', '<c-p>', '<cmd>Obsidian quick_switch<cr>', { desc = 'Obsidian Quick Switch', buffer = bufnr, remap = false })
