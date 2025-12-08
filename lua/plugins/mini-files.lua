0f45141ef3180bee73534359a0721399490af79a
Bryant Paz <pazbryant7@outlook.com>
2025-12-07 21:16:31 -0600

    feat(mini-files): update default binds

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -3,10 +3,25 @@ return {
 	lazy = false,
 	version = false,
 	config = function()
-		require('mini.files').setup()
+		require('mini.files').setup({
+			mappings = {
+				close = 'q',
+				go_in = 'l',
+				go_in_plus = '<cr>',
+				go_out = 'h',
+				go_out_plus = '-',
+				mark_goto = "'",
+				mark_set = 'm',
+				reset = '<BS>',
+				reveal_cwd = '@',
+				show_help = 'g?',
+				synchronize = 's',
+				trim_left = '<',
+				trim_right = '>',
+			},
+		})
 
 		local map = vim.keymap.set
-
 		map('n', '-', function()
 			require('mini.files').open(vim.api.nvim_buf_get_name(0))
 		end, { desc = 'Mini Files Open' })
