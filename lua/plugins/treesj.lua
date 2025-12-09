54cc4b4e7174cd45d9fccbee9b3f4a71049a55e8
Bryant Paz <pazbryant7@outlook.com>
2025-12-09 12:45:58 -0600

    feat(treesj): new plugin added

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,12 @@
+return {
+	'Wansmer/treesj',
+	dependencies = { 'nvim-treesitter/nvim-treesitter' },
+	config = function()
+		require('treesj').setup()
+	end,
+	keys = {
+    '<space>m',
+    '<space>j',
+    '<space>s',
+  },
+}
