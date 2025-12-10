b860b8de4e459598c422ce01a5fb45e0e363793e
Bryant Paz <pazbryant7@outlook.com>
2025-12-10 16:13:44 -0600

    feat(oversee): adding lazy binds

--- lua/plugins/oversee.lua
+++ lua/plugins/oversee.lua
@@ -4,4 +4,8 @@ return {
 	---@module 'overseer'
 	---@type overseer.SetupOpts
 	opts = {},
+	keys = {
+		{ '<leader>or', '<cmd>OverseerRun<cr>', desc = 'Overseer run' },
+		{ '<leader>ot', '<cmd>OverseerToggle<cr>', desc = 'Overseer toggle' },
+	},
 }
