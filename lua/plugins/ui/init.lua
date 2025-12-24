5a6b60d8085b5af2ce49c42ae66f30abfd349b8e
Bryant Paz <pazbryant7@outlook.com>
2025-12-23 20:31:31 -0600

    feat(colorschemes,ui): remove unused ones

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -1,35 +1,15 @@
 return {
-	{
-		'EdenEast/nightfox.nvim',
-		config = function()
-			require('nightfox').setup()
-		end,
-	},
-
-	{
-		'Tsuzat/NeoSolarized.nvim',
-		opts = {
-			transparent = false,
+	'catppuccin/nvim',
+	name = 'catppuccin',
+	opts = {
+		flavour = 'mocha',
+		background = { -- :h background
+			light = 'latte',
+			dark = 'mocha',
 		},
-		config = function(_, opts)
-			require('NeoSolarized').setup(opts)
-			vim.cmd([[ colorscheme NeoSolarized ]])
-		end,
+		no_italic = true, -- Force no italic
 	},
-
-	{
-		'catppuccin/nvim',
-		name = 'catppuccin',
-		opts = {
-			flavour = 'mocha',
-			background = { -- :h background
-				light = 'latte',
-				dark = 'mocha',
-			},
-			no_italic = true, -- Force no italic
-		},
-		config = function(_, opts)
-			require('catppuccin').setup(opts)
-		end,
-	},
+	config = function(_, opts)
+		require('catppuccin').setup(opts)
+	end,
 }
