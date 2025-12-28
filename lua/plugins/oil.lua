1cac442030407166894a1b266089500d0da30860
Bryant Paz <pazbryant7@outlook.com>
2025-12-28 14:01:41 -0600

    feat(oil): update defaults

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,13 +8,10 @@ return {
 		keymaps = {
 			['g?'] = { 'actions.show_help', mode = 'n' },
 			['<CR>'] = 'actions.select',
-			['<C-v>'] = 'actions.preview',
 			['q'] = { 'actions.close', mode = 'n' },
 			['<C-l>'] = 'actions.refresh',
 			['-'] = { 'actions.parent', mode = 'n' },
 			['_'] = { 'actions.open_cwd', mode = 'n' },
-			['`'] = { 'actions.cd', mode = 'n' },
-			['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
 			['gs'] = { 'actions.change_sort', mode = 'n' },
 			['gx'] = 'actions.open_external',
 			['.'] = { 'actions.toggle_hidden', mode = 'n' },
