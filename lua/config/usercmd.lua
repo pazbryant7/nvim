0f89e28fcbb130d28b01f81fba9d37046e9399a9
Bryant Paz <pazbryant7@outlook.com>
2026-05-21 17:20:14 -0600

    feat(stylua): update column up to 120

--- lua/config/usercmd.lua
+++ lua/config/usercmd.lua
@@ -24,22 +24,14 @@ end, {
 
 usercmd('ToggleSpell', function()
 	vim.o.spell = not vim.o.spell
-	vim.notify(
-		'spell is ' .. (vim.o.spell and 'on' or 'off'),
-		vim.log.levels.INFO,
-		{ title = 'Neovim Alert' }
-	)
+	vim.notify('spell is ' .. (vim.o.spell and 'on' or 'off'), vim.log.levels.INFO, { title = 'Neovim Alert' })
 end, { desc = 'Spell toggle' })
 
 usercmd('ToggleDiagnostics', function()
 	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 	local is_enabled = vim.diagnostic.is_enabled()
 	local msg = is_enabled and 'enabled' or 'disabled'
-	vim.notify(
-		string.format('Diagnostic has been %s', msg),
-		vim.log.levels.INFO,
-		{ title = 'Neovim Alert' }
-	)
+	vim.notify(string.format('Diagnostic has been %s', msg), vim.log.levels.INFO, { title = 'Neovim Alert' })
 end, { desc = 'Diagnostics toggle' })
 
 -- tmux
@@ -48,45 +40,27 @@ local function open_in_tmux_split(file)
 	local cwd = vim.fn.getcwd()
 	local cmd_parts = {}
 	if vim.env.NVIM_APPNAME then
-		table.insert(
-			cmd_parts,
-			'NVIM_APPNAME=' .. vim.fn.shellescape(vim.env.NVIM_APPNAME)
-		)
+		table.insert(cmd_parts, 'NVIM_APPNAME=' .. vim.fn.shellescape(vim.env.NVIM_APPNAME))
 	end
 	table.insert(cmd_parts, vim.v.progname)
 	table.insert(cmd_parts, vim.fn.shellescape(file))
 
-	local tmux_cmd = string.format(
-		"tmux split-window -h -c %s '%s'",
-		vim.fn.shellescape(cwd),
-		table.concat(cmd_parts, ' ')
-	)
+	local tmux_cmd =
+		string.format("tmux split-window -h -c %s '%s'", vim.fn.shellescape(cwd), table.concat(cmd_parts, ' '))
 	local result = vim.fn.system(tmux_cmd)
 	if vim.v.shell_error ~= 0 then
-		vim.notify(
-			'Failed to open tmux pane: ' .. result,
-			vim.log.levels.ERROR,
-			{ title = 'Tmux' }
-		)
+		vim.notify('Failed to open tmux pane: ' .. result, vim.log.levels.ERROR, { title = 'Tmux' })
 	end
 end
 
 usercmd('GoToFileWithTmux', function()
 	if not vim.env.TMUX then
-		vim.notify(
-			'Not inside a tmux session.',
-			vim.log.levels.ERROR,
-			{ title = 'Tmux' }
-		)
+		vim.notify('Not inside a tmux session.', vim.log.levels.ERROR, { title = 'Tmux' })
 		return
 	end
 	local file = vim.fn.expand('<cfile>')
 	if file == '' then
-		vim.notify(
-			'No file path under cursor.',
-			vim.log.levels.WARN,
-			{ title = 'Tmux' }
-		)
+		vim.notify('No file path under cursor.', vim.log.levels.WARN, { title = 'Tmux' })
 		return
 	end
 	open_in_tmux_split(file)
