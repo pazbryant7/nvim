eff1054e37961b8640ade34dc89bdc4bf20d01d0
Bryant Paz <pazbryant7@outlook.com>
2026-06-13 00:44:42 -0600

    feat(miniai): update default config

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -1,12 +1,14 @@
 return {
 	'echasnovski/mini.ai',
 	version = false,
-	dependencies = 'nvim-treesitter-textobjects',
-	config = function()
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter-textobjects',
+	},
+	opts = function()
 		local mini_ai = require('mini.ai')
 		local gen_spec = mini_ai.gen_spec
 
-		mini_ai.setup({
+		return {
 			n_lines = 500,
 			search_method = 'cover_or_next',
 			mappings = {
@@ -19,22 +21,31 @@ return {
 				goto_left = '[g',
 				goto_right = ']g',
 			},
+
 			custom_textobjects = {
 				o = gen_spec.treesitter({ a = '@loop.outer', i = '@loop.inner' }),
 				c = gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }),
 				F = gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
-				i = gen_spec.treesitter({ a = '@conditional.outer', i = '@conditional.inner' }),
+				I = gen_spec.treesitter({ a = '@conditional.outer', i = '@conditional.inner' }),
+				g = function()
+					local from = { line = 1, col = 1 }
+					local to = {
+						line = vim.fn.line('$'),
+						col = math.max(vim.fn.getline('$'):len(), 1),
+					}
+					return { from = from, to = to }
+				end,
 			},
-		})
+		}
 	end,
 	keys = {
-		{ '[g', mode = { 'n' }, desc = 'Mini.AI Go left' },
-		{ ']g', mode = { 'n' }, desc = 'Mini.AI Go right' },
-		{ 'a', mode = { 'x', 'o' }, desc = 'Mini.AI Around Textobject' },
-		{ 'i', mode = { 'x', 'o' }, desc = 'Mini.AI Inside Textobject' },
-		{ 'an', mode = { 'x', 'o' }, desc = 'Mini.AI Around Next Textobject' },
-		{ 'il', mode = { 'x', 'o' }, desc = 'Mini.AI Inside Last Textobject' },
-		{ 'al', mode = { 'x', 'o' }, desc = 'Mini.AI Around Last Textobject' },
-		{ 'in', mode = { 'x', 'o' }, desc = 'Mini.AI Inside Next Textobject' },
+		{ 'il', mode = { 'x', 'o' }, desc = 'Mini.AI: inside last' },
+		{ 'al', mode = { 'x', 'o' }, desc = 'Mini.AI: around last' },
+		{ 'in', mode = { 'x', 'o' }, desc = 'Mini.AI: inside next' },
+		{ 'an', mode = { 'x', 'o' }, desc = 'Mini.AI: around next' },
+		{ 'i', mode = { 'x', 'o' }, desc = 'Mini.AI: inside textobject' },
+		{ 'a', mode = { 'x', 'o' }, desc = 'Mini.AI: around textobject' },
+		{ '[g', mode = { 'n', 'x', 'o' }, desc = 'Mini.AI: go to left edge' },
+		{ ']g', mode = { 'n', 'x', 'o' }, desc = 'Mini.AI: go to right edge' },
 	},
 }
