78fec320962a606ca9702be0c537238bc818ef86
Bryant Paz <pazbryant7@outlook.com>
2026-06-21 11:45:42 -0600

    feat(nvim-web-icons): remove unused plugin

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -3,7 +3,6 @@ return {
 	ft = 'markdown',
 	dependencies = {
 		'nvim-treesitter/nvim-treesitter',
-		'nvim-tree/nvim-web-devicons',
 	},
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
