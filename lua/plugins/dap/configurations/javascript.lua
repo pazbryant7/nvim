39274730ec73f1400e05055b5f15686bacb4dbbb
Bryant Paz <pazbryant7@outlook.com>
2026-06-23 11:35:56 -0600

    feat(dap): update configuration
    - use node instead of tsx
    - remove unused code

--- lua/plugins/dap/configurations/javascript.lua
+++ lua/plugins/dap/configurations/javascript.lua
@@ -1,18 +1,11 @@
 local M = {}
 
-local function get_project_root()
-	local root_pattern = vim.fs.find({ '.git', 'package.json' }, { upward = true, stop = vim.env.HOME })[1]
-	return root_pattern and vim.fs.dirname(root_pattern) or vim.fn.getcwd()
-end
-
-local shared_skip_files = {
+local skip = {
 	'<node_internals>/**',
 	'node_modules/**',
 }
 
-local project_root = get_project_root()
-
-local common_config = {
+local common = {
 	{
 		name = 'Launch Chrome with "localhost"',
 		type = 'pwa-chrome',
@@ -20,10 +13,9 @@ local common_config = {
 		url = function()
 			return vim.fn.input('Enter URL: ', 'http://localhost:3000')
 		end,
-		webRoot = project_root,
 		sourceMaps = true,
 		protocol = 'inspector',
-		skipFiles = shared_skip_files,
+		skipFiles = skip,
 	},
 	{
 		name = 'Attach to process (select pid)',
@@ -32,60 +24,54 @@ local common_config = {
 		processId = function()
 			return vim.fn.input('Enter PID: ')
 		end,
-		cwd = project_root,
-		skipFiles = shared_skip_files,
+		skipFiles = skip,
 	},
 }
 
-local javascript_config = {
+local javascript = {
+	{
+		skipFiles = skip,
+		type = 'pwa-node',
+		sourceMaps = true,
+		request = 'launch',
+		program = '${file}',
+		runtimeExecutable = 'node',
+		name = 'Launch Current File (node)',
+	},
+}
+
+local typescript = {
 	{
 		name = 'Launch Current File (node)',
 		type = 'pwa-node',
 		request = 'launch',
 		program = '${file}',
-		cwd = project_root,
-		sourceMaps = true,
 		runtimeExecutable = 'node',
-		skipFiles = shared_skip_files,
-	},
-}
-
-local typescript_config = {
-	{
-		name = 'Launch Current File (tsx)',
-		type = 'pwa-node',
-		request = 'launch',
-		program = '${file}',
-		cwd = project_root,
-		runtimeExecutable = 'tsx',
 		sourceMaps = true,
-		skipFiles = shared_skip_files,
+		skipFiles = skip,
 	},
 }
 
-local test_config = {}
-
-local language_configs = {
-	javascript = javascript_config,
-	typescript = typescript_config,
-}
-
-local language_aliases = {
+local aliases = {
 	javascriptreact = 'javascript',
 	typescriptreact = 'typescript',
 }
 
+local configs = {
+	javascript = javascript,
+	typescript = typescript,
+}
+
 M.configurations = {}
 
-for lang, specific_config in pairs(language_configs) do
-	local final_config = vim.deepcopy(common_config)
-	vim.list_extend(final_config, specific_config)
-	vim.list_extend(final_config, test_config)
-	M.configurations[lang] = final_config
+for lang, config in pairs(configs) do
+	local base = vim.deepcopy(common)
+	vim.list_extend(base, config)
+	M.configurations[lang] = base
 end
 
-for new_lang, existing_lang in pairs(language_aliases) do
-	M.configurations[new_lang] = M.configurations[existing_lang]
+for type, lang in pairs(aliases) do
+	M.configurations[type] = M.configurations[lang]
 end
 
 return M.configurations
