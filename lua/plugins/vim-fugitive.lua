cda46e34c984344019e7423f313a89ce5e97251d
Bryant Paz <pazbryant7@outlook.com>
2026-07-19 06:00:12 -0600

    feat(fugitive): new plugin

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,11 +1,9 @@
-local function has_git_folder()
-	return vim.fn.isdirectory('.git') == 1
-end
-
 return {
 	'tpope/vim-fugitive',
-	cond = has_git_folder,
 	cmd = { 'G', 'Git', 'Gclog', 'Gdiffsplit' },
+	cond = function()
+		return vim.fn.isdirectory('.git') == 1
+	end,
 	keys = {
 		{
 			'<c-g>',
@@ -15,77 +13,8 @@ return {
 			mode = 'n',
 			desc = 'Fugitive: Git status (summary window)',
 		},
-		{
-			'<leader>gs',
-			function()
-				vim.cmd('Git status --short')
-			end,
-			mode = 'n',
-			desc = 'Fugitive: Git status short',
-		},
-		{
-			'<leader>ga',
-			function()
-				vim.cmd('Git add %')
-			end,
-			mode = 'n',
-			desc = 'Fugitive: Stage current file',
-		},
-		{
-			'<leader>gA',
-			function()
-				vim.cmd('Git add -A')
-			end,
-			mode = 'n',
-			desc = 'Fugitive: Stage all files',
-		},
-		{
-			'<leader>gu',
-			function()
-				vim.cmd('Git restore --staged %')
-			end,
-			mode = 'n',
-			desc = 'Fugitive: Unstage current file',
-		},
-		{
-			'<leader>gc',
-			function()
-				vim.cmd('Git commit')
-			end,
-			mode = 'n',
-			desc = 'Fugitive: Commit',
-		},
-		{
-			'<leader>gl',
-			function()
-				vim.cmd('Gclog')
-			end,
-			mode = 'n',
-			desc = 'Fugitive: Log (populate quickfix)',
-		},
-		{
-			'<leader>gL',
-			function()
-				vim.cmd('Gclog -- %')
-			end,
-			mode = 'n',
-			desc = 'Fugitive: Log for current file',
-		},
+		{ '<leader>gb', ':Git blame<CR>', mode = { 'n', 'v' }, desc = 'Git Blame' },
 	},
-
-	init = function()
-		vim.api.nvim_create_autocmd('BufReadPost', {
-			pattern = 'COMMIT_EDITMSG',
-			desc = 'Close split after commit',
-			callback = function()
-				vim.opt_local.spell = true
-				vim.opt_local.spelllang = 'en_us'
-				vim.opt_local.textwidth = 72
-				vim.cmd('normal! gg')
-			end,
-		})
-	end,
-
 	config = function()
 		vim.g.fugitive_summary_format = '%d %s (%cr) <%an>'
 	end,
