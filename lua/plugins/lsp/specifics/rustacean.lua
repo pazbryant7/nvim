ca6ca0b36e84309af13f8c285ebd272f48411ca1
Bryant Paz <pazbryant7@outlook.com>
2026-08-12 15:10:46 -0600

    feat(rustacean,typsecript-tools): only start with needed dependencies

--- lua/plugins/lsp/specifics/rustacean.lua
+++ lua/plugins/lsp/specifics/rustacean.lua
@@ -1,8 +1,8 @@
 return {
 	'mrcjkb/rustaceanvim',
-  enabled = function()
-    return os.getenv('IN_NIX_SHELL') ~= nil
-  end,
+	enabled = function()
+		return require('config.utils').executables_exist('rust-analyzer', 'cargo')
+	end,
 	version = '^6',
 	ft = 'rust',
 	config = function()
